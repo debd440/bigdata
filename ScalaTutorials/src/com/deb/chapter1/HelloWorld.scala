4804421182f6a5178ecdb75c1e625a875716559a
Debdutta Bhattacharya <deb@jojodelivery.com>
2019-09-03 09:08:39 +0530

    Modified Hello World Code

--- ScalaTutorials/src/com/deb/chapter1/HelloWorld.scala
+++ ScalaTutorials/src/com/deb/chapter1/HelloWorld.scala
@@ -2,8 +2,11 @@ package com.deb.chapter1
 
 object HelloWorld {
 
+  println("Above Main Method")
   def main(args: Array[String]): Unit = {
 
     print("Hello World")
+
   }
+  println("Execution Completed")
 }
